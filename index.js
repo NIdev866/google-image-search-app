7d1eae2ee186e42ba58057d3c7202d0fb2e84667
NIdev866 <nidev866@gmail.com>
2017-08-30 11:37:17 +0100

    fixed offset functionality

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const imageSearch = require("google-images")
 const imageSearchClient = new imageSearch(process.env.CSE_ID, process.env.IMAGE_KEY)
 
 app.get("/api/imagesearch/:mySearch", (req, res)=>{
-  const offset = req.query.offset ? req.query.offset : 10;
+  const offset = req.query.offset ? req.query.offset : 1;
   const searchQuery = req.params.mySearch;
   const date = new Date().toISOString();
   imageSearchClient.search(searchQuery,{
-    page: (offset/10)
+    page: (offset)
   }).then(function(results){
     if(results.length > 0){
           const imgSearchList = imageHistory({
