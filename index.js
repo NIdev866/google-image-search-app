296c1d5b6b9e29d72798df0e2e9f41f28e58a1eb
NIdev866 <nidev866@gmail.com>
2017-08-30 11:31:22 +0100

    project working

--- index.js
+++ index.js
@@ -4,10 +4,39 @@ const path = require("path")
 require("dotenv").config()
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGOLAB_URI)
+const imageHistory = require("./models/Image_history.model.js")
+const imageSearch = require("google-images")
+const imageSearchClient = new imageSearch(process.env.CSE_ID, process.env.IMAGE_KEY)
 
-const api = require("./routes/api")
+app.get("/api/imagesearch/:mySearch", (req, res)=>{
+  const offset = req.query.offset ? req.query.offset : 10;
+  const searchQuery = req.params.mySearch;
+  const date = new Date().toISOString();
+  imageSearchClient.search(searchQuery,{
+    page: (offset/10)
+  }).then(function(results){
+    if(results.length > 0){
+          const imgSearchList = imageHistory({
+            term: searchQuery,
+            when: date
+          })
+          imgSearchList.save((err)=>{
+            if(err) res.redirect("/")
+          })
+          res.json(results);
+    } 
+    else {
+      res.json('invalid search');
+    }
+  });
+})
 
-app.use("/api", api)
+app.get("/api/latest/imagesearch", (req, res)=>{
+  imageHistory.find({}, (err, searches)=>{
+    if(err) res.redirect("/")
+    res.json(searches)
+  })
+})
 
 app.get("/", (req, res)=>{
   res.sendFile(path.join(__dirname, "static/index.html"))
